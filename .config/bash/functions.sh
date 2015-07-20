56800bbcae078c4e0576a0d0b7ae3508bd90dfdb
Guillaume Friloux <guillaume@friloux.me>
2015-07-20 11:00:17 +0200

    Small changes for share() bash function

--- .config/bash/functions.sh
+++ .config/bash/functions.sh
@@ -12,13 +12,13 @@ share()
 {
    if [ $# -eq 0 ]; then
       echo "No arguments specified. Usage:"
-      echo "transfer /tmp/test.md"
-      echo "cat /tmp/test.md | transfer test.md"
+      echo "share /tmp/test.md"
+      echo "cat /tmp/test.md | share test.md"
       return 1
    fi
 
    # write to output to tmpfile because of progress bar
-   tmpfile=$( mktemp -t transferXXX );
+   tmpfile=$(mktemp -t transferXXX );
 
    if tty -s; then
       basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g');
