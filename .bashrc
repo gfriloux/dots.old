5d400f6378b0a63f0345face1fa3011e3c06b89b
Guillaume Friloux <guillaume@friloux.me>
2015-07-20 10:57:27 +0200

    Updating bash configuration

--- .bashrc
+++ .bashrc
@@ -2,14 +2,18 @@
 
 alias xterm="xterm -fa inconsolata-9"
 alias ls="/usr/bin/vendor_perl/ls++"
-alias df="dfc"
-alias du="ncdu"
 alias nano="nano -c"
-
+alias mount='mount | column -t'
 
 export TERM="xterm-256color"
 export PATH="/usr/lib/colorgcc/bin/:$PATH"
 export CCACHE_PATH="/usr/bin" 
+
+export VISUAL=nano
 export EDITOR="nano"
 
-PS1='\[\033[01;34m\]> \[\033[00m\]'
+export LANG=fr_FR.UTF-8
+
+MANPAGER="/usr/bin/most -s"
+
+#PS1='\[\033[01;34m\]> \[\033[00m\]'
---
+++ .config/bash/functions.sh
@@ -0,0 +1,33 @@
+lps()
+{
+   tail -f $1 | perl -e 'while (<>) {$l++;if (time > $e) {$e=time;print "$l\n";$l=0}}'
+}
+
+paste()
+{
+   curl -F 'sprunge=<-' http://sprunge.us
+}
+
+share()
+{
+   if [ $# -eq 0 ]; then
+      echo "No arguments specified. Usage:"
+      echo "transfer /tmp/test.md"
+      echo "cat /tmp/test.md | transfer test.md"
+      return 1
+   fi
+
+   # write to output to tmpfile because of progress bar
+   tmpfile=$( mktemp -t transferXXX );
+
+   if tty -s; then
+      basefile=$(basename "$1" | sed -e 's/[^a-zA-Z0-9._-]/-/g');
+      curl --progress-bar --upload-file "$1" "https://transfer.sh/$basefile" >> $tmpfile;
+   else
+      curl --progress-bar --upload-file "-" "https://transfer.sh/$1"
+   fi
+
+   cat $tmpfile; rm -f $tmpfile;
+}
+
+
